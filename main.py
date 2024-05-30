3e1d3bb956b92e34a31311f2066d083d18de2af1
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-05-30 19:33:53 +0200

    added logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import simpy
+import logging
 
 # Time is in minutes!!!
 
@@ -9,6 +10,11 @@ AVG_SUPPORT_TIME = 5
 CUSTOMER_INTERVAL = 2
 SIM_TIME = 120
 
+# Configure logging
+LOGGING_ENABLED = False
+if LOGGING_ENABLED:
+    logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('CallCenterSimulation')
 
 customer_handled = 0
 
@@ -21,33 +27,40 @@ class CallCenter:
     def support(self, customer):
         random_time = max(1, np.random.normal(self.avg_support_time, 4))
         yield self.env.timeout(random_time)
-        print(f"Support finished for {customer} at {self.env.now:.2f}")
-
+        if LOGGING_ENABLED:
+            logger.info(f"Support finished for {customer} at {self.env.now:.2f}")
 
 def customer(env: simpy.Environment, name, call_center: CallCenter):
     global customer_handled
-    print(f"Customer {name} enters waiting queue at {env.now:.2f}")
+    if LOGGING_ENABLED:
+        logger.info(f"Customer {name} enters waiting queue at {env.now:.2f}")
     with call_center.staff.request() as request:
         yield request
-        print(f"Customer {name} enters call at {env.now:.2f}")
+        if LOGGING_ENABLED:
+            logger.info(f"Customer {name} enters call at {env.now:.2f}")
         yield env.process(call_center.support(name))
-        print(f"Customer {name} leaves call at {env.now:.2f}")
+        if LOGGING_ENABLED:
+            logger.info(f"Customer {name} leaves call at {env.now:.2f}")
         customer_handled += 1
-        
+
 def setup(env: simpy.Environment, num_employees, avg_support_time, customer_interval):
     call_center = CallCenter(env, num_employees, avg_support_time)
-    
+
     for i in range(1, 6):
         env.process(customer(env, i, call_center))
-        
+
     while True:
         yield env.timeout(random.randint(customer_interval - 1, customer_interval + 1))
         i += 1
         env.process(customer(env, i, call_center))
-    
-print("Call Center Simulation")
-env = simpy.Environment()
-env.process(setup(env, NUM_EMPLOYEES, AVG_SUPPORT_TIME, CUSTOMER_INTERVAL))
-env.run(until=SIM_TIME)
 
-print("Customer handled: ", customer_handled)
+    
+for _ in range(100):
+    env = simpy.Environment()
+    env.process(setup(env, NUM_EMPLOYEES, AVG_SUPPORT_TIME, CUSTOMER_INTERVAL))
+    env.run(until=SIM_TIME)
+
+if LOGGING_ENABLED:
+    logger.info(f"Customer handled on average: {customer_handled / 100}")
+else:
+    print(f"Customer handled on average at Call Center: {customer_handled / 100}")
