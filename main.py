e7f36b4a146b96e91563e249b63976130bbe90a0
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-06-04 19:04:45 +0200

    update

--- main.py
+++ main.py
@@ -12,18 +12,13 @@ SIM_TIME = 120
 PATIENCE = [1, 3]
 
 # Configure logging
-LOGGING_ENABLED = True
-if LOGGING_ENABLED:
-    logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("CallCenterSimulator")
 
-customer_handled = 0
-impatient_customers = 0
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Call Center Simulator")
 
 
 def log_info(logger: logging.Logger = logger, text: str = ""):
-    if LOGGING_ENABLED:
-        logger.info(text)
+    logger.info(text)
 
 
 def log_warning(logger: logging.Logger = logger, text: str = ""):
@@ -42,15 +37,15 @@ class CallCenter:
         log_info(text=f"Support finished for {customer} at {self.env.now:.2f}")
 
 
-def customer(env: simpy.Environment, name, call_center: CallCenter):
+def customer(env: simpy.Environment, name, call_center: CallCenter, patience: list):
     global customer_handled
     global impatient_customers
-    patience = np.random.uniform(PATIENCE[0], PATIENCE[1])
+    random_patience = np.random.uniform(patience[0], patience[1])
     arrival_time = env.now
     log_info(
-        text=f"Customer {name} enters waiting queue at {env.now:.2f} with patience {patience:.2f} minutes")
+        text=f"Customer {name} enters waiting queue at {env.now:.2f} with patience {random_patience:.2f} minutes")
     with call_center.staff.request() as request:
-        results = yield request | env.timeout(patience)
+        results = yield request | env.timeout(random_patience)
         wait_time = env.now - arrival_time
         if request in results:
             log_info(
@@ -64,21 +59,34 @@ def customer(env: simpy.Environment, name, call_center: CallCenter):
             impatient_customers += 1
 
 
-def setup(env: simpy.Environment, num_employees, avg_support_time, customer_interval):
+def setup(env: simpy.Environment, num_employees, avg_support_time, customer_interval, patience):
     call_center = CallCenter(env, num_employees, avg_support_time)
 
     for i in range(1, 6):
-        env.process(customer(env, i, call_center))
+        env.process(customer(env, i, call_center, patience))
 
     while True:
         yield env.timeout(random.randint(customer_interval - 1, customer_interval + 1))
         i += 1
-        env.process(customer(env, i, call_center))
+        env.process(customer(env, i, call_center, patience))
 
 
-env = simpy.Environment()
-env.process(setup(env, NUM_EMPLOYEES, AVG_SUPPORT_TIME, CUSTOMER_INTERVAL))
-env.run(until=SIM_TIME)
+def run_simulation(num_employees: int, avg_support_time: int, customer_interval: int, patience: list, sim_time: int):
+    global customer_handled
+    global impatient_customers
 
-print(f"Total customers handled: {customer_handled}")
-print(f"Total impatient customers: {impatient_customers}")
+    customer_handled = 0
+    impatient_customers = 0
+
+    env = simpy.Environment()
+    env.process(setup(env, num_employees, avg_support_time,
+                customer_interval, patience))
+    env.run(until=sim_time)
+    return customer_handled, impatient_customers
+
+
+customer_handled, impatient_customers = run_simulation(num_employees=2, avg_support_time=5, customer_interval=3,
+                                                       sim_time=120, patience=[1, 3])
+
+print(f"Customer handled: {customer_handled}")
+print(f"Impatient customers: {impatient_customers}")
---
+++ readme.md
@@ -0,0 +1,4 @@
+https://vcc.live/call-center-kpis/average-wait-time/#:~:text=Inbound%20call%20centers%20typically%20aim,of%20less%20than%201%20minute.
+
+
+`Inbound call centers typically aim for an Average Wait Time of around 30 seconds to 2 minutes. This ensures that customers are attended to promptly and their issues are addressed in a timely manner. Customer support hotlines often target an Average Wait Time of less than 1 minute.`
