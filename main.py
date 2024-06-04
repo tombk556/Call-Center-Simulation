1bf401fb878b4b04b0c2423344955f5091220433
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-06-04 19:24:10 +0200

    added logging

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import random
 import numpy as np
 import simpy
 import logging
+import os
 
 # Time is in minutes!!!
 
-NUM_EMPLOYEES = 2
-AVG_SUPPORT_TIME = 5
-CUSTOMER_INTERVAL = 3
-SIM_TIME = 120
-PATIENCE = [1, 3]
-
-# Configure logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("Call Center Simulator")
+logger.propagate = False  # To prevent logging to console by root logger
 
+
+file_handler = logging.FileHandler("call_center_log.log", mode="w")
+file_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 def log_info(logger: logging.Logger = logger, text: str = ""):
     logger.info(text)
@@ -42,20 +45,17 @@ def customer(env: simpy.Environment, name, call_center: CallCenter, patience: li
     global impatient_customers
     random_patience = np.random.uniform(patience[0], patience[1])
     arrival_time = env.now
-    log_info(
-        text=f"Customer {name} enters waiting queue at {env.now:.2f} with patience {random_patience:.2f} minutes")
+    log_info(text=f"Customer {name} enters waiting queue at {env.now:.2f} with patience {random_patience:.2f} minutes")
     with call_center.staff.request() as request:
         results = yield request | env.timeout(random_patience)
         wait_time = env.now - arrival_time
         if request in results:
-            log_info(
-                text=f"Customer {name} enters call at {env.now:.2f} after waiting {wait_time:.2f} minutes")
+            log_info(text=f"Customer {name} enters call at {env.now:.2f} after waiting {wait_time:.2f} minutes")
             yield env.process(call_center.support(name))
             log_info(text=f"Customer {name} leaves call at {env.now:.2f}")
             customer_handled += 1
         else:
-            log_warning(
-                text=f"Customer {name} leaves queue after waiting more than {wait_time:.2f} minutes")
+            log_warning(text=f"Customer {name} leaves queue after waiting more than {wait_time:.2f} minutes")
             impatient_customers += 1
 
 
@@ -77,16 +77,13 @@ def run_simulation(num_employees: int, avg_support_time: int, customer_interval:
 
     customer_handled = 0
     impatient_customers = 0
+    
+    file_handler.close()
+    os.remove("call_center_log.log")
 
     env = simpy.Environment()
     env.process(setup(env, num_employees, avg_support_time,
                 customer_interval, patience))
     env.run(until=sim_time)
+    log_info(text=f"Simulation Results: {customer_handled} customers handled, {impatient_customers} impatient customers")
     return customer_handled, impatient_customers
-
-
-customer_handled, impatient_customers = run_simulation(num_employees=2, avg_support_time=5, customer_interval=3,
-                                                       sim_time=120, patience=[1, 3])
-
-print(f"Customer handled: {customer_handled}")
-print(f"Impatient customers: {impatient_customers}")
